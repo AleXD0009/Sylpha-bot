22e6ba936a3e9a87c3d4206045357b9c85801752
J̶T̶X̶S̶ <145930757+AleXD0009@users.noreply.github.com>
2024-06-26 15:38:10 -0300

    𝙎𝙮𝙡𝙥𝙝𝙖 - 𝘽𝙤𝙩

---
+++ plugins/_fakeReply.js
@@ -0,0 +1,36 @@
+import fetch from 'node-fetch'
+
+export async function before(m, { conn }) {
+let img = await (await fetch(`https://tinyurl.com/2c5hk765`)).buffer()
+
+ global.rcanal = {
+    contextInfo: {
+    	isForwarded: true,
+    forwardedNewsletterMessageInfo: {
+      newsletterJid: id_canal,
+      serverMessageId: 100,
+      newsletterName: name_canal,
+    },
+    },
+  }
+
+ global.adReply = {
+	    contextInfo: { 
+             forwardingScore: 9999, 
+                 isForwarded: false, 
+                    externalAdReply: {
+				    showAdAttribution: true,
+					title: botname,
+					body: textbot,
+					mediaUrl: null,
+					description: null,
+					previewType: "PHOTO",
+					thumbnailUrl: img,
+                    thumbnail: img,
+		           sourceUrl: canal,
+		           mediaType: 1,
+                   renderLargerThumbnail: true
+				}
+			}
+		}
+}
