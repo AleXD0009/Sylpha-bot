8460ae0c32aa1fd9e0096275f942684b5626eb61
J̶T̶X̶S̶ <145930757+AleXD0009@users.noreply.github.com>
2024-07-14 12:30:19 -0300

    𝙎𝙮𝙡𝙥𝙝𝙖 - 𝘽𝙤𝙩

--- plugins/dl-facebook.js
+++ plugins/dl-facebook.js
@@ -1,27 +1,25 @@
-
 import fetch from 'node-fetch';
 
 let handler = async (m, { conn, args, usedPrefix, command }) => {
   if (!args[0]) return m.reply('*`Ingresa un enlace de facebook`*');
 
-    try {
-        const apiResponse = await fetch(`https://delirius-api-oficial.vercel.app/api/tiktok?url=${args[0]}`);
-        const responseData = await apiResponse.json();
-        
-        if (responseData.status) {
-            const { data } = responseData;
-            const { title, links } = data;
-        let txt = '    `Facebook downloader`\n\n';
-            txt += `> *Titulo* : _${title}_\n`;
+  try {
+    const apiResponse = await fetch(`https://delirius-api-oficial.vercel.app/api/tiktok?url=${args[0]}`);
+    const responseData = await apiResponse.json();
+    
+    if (responseData.success) {
+      const { title, links } = responseData;
+      let txt = '    `Facebook downloader`\n\n';
+      txt += `> *Titulo* : _${title}_\n`;
 
-            await conn.sendMessage(m.chat, { video: { url: links.sd_0.link;}, caption: txt }, { quoted: m });
-        } else {
-        }
-    } catch {
+      await conn.sendMessage(m.chat, { video: { url: links[0].link }, caption: txt }, { quoted: m });
+    } else {
     }
+  } catch {
+  }
 }
 
 handler.help = ['facebook *<link>*'];
 handler.tags = ['downloader'];
 handler.command = ['fb', 'facebook'];
-export default handler
+export default handler;
