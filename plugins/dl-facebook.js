fbd3ada03150d71fedd99cff113f112f5fbb2ee5
J̶T̶X̶S̶ <145930757+AleXD0009@users.noreply.github.com>
2024-07-14 11:41:15 -0300

    𝙎𝙮𝙡𝙥𝙝𝙖 - 𝘽𝙤𝙩

--- plugins/dl-facebook.js
+++ plugins/dl-facebook.js
@@ -1,14 +1,31 @@
-import Starlights from '@StarlightsTeam/Scraper'
+import { snapsave } from '@bochilteam/scraper';
 
 let handler = async (m, { conn, args, usedPrefix, command }) => {
-if (!args || !args[0]) return conn.reply(m.chat, '*`Ingresa un link de facebook`*', m)
-try {
-let { title, SD, HD } = await Starlights.fbdl(args[0])
-await conn.sendFile(m.chat, SD || HD, 'fbdl.mp4', `> *Título* : ${title}`, m, null, rcanal)
-} catch {
-}}
-handler.help = ['fb *<link>*']
-handler.tags = ['dl'] 
-handler.command = /^(facebook|fb|facebookdl|fbdl)$/i
+    if (!args || args.length === 0) {
+        return conn.reply(m.chat, '*`Ingresa un link de facebook`*', m);
+    }
 
-export default handler
+    try {
+        const link = args[0];
+        const data = await snapsave(link);
+        
+        if (!data) {
+            return conn.reply(m.chat, 'No se encontró ningún video para ese enlace.', m);
+        }
+
+        const { title, SD, HD } = data;
+
+        if (!SD && !HD) {
+            return conn.reply(m.chat, 'No se encontró ningún enlace de descarga válido para ese video.', m);
+        }
+      
+        await conn.sendFile(m.chat, SD || HD, 'fbdl.mp4', `> *Título*: ${title}`, m, null, rcanal);
+    } catch {
+    }
+};
+
+handler.help = ['facebook *<link>*'];
+handler.tags = ['dl'];
+handler.command = /^(facebook|fb|facebookdl|fbdl)$/i;
+
+export default handler;
