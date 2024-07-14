2b494bec2b6fe3c60b26de8985543bb202162660
J̶T̶X̶S̶ <145930757+AleXD0009@users.noreply.github.com>
2024-07-14 12:26:27 -0300

    𝙎𝙮𝙡𝙥𝙝𝙖 - 𝘽𝙤𝙩

--- plugins/dl-facebook.js
+++ plugins/dl-facebook.js
@@ -1,74 +1,35 @@
+
 import fetch from 'node-fetch';
 
 let handler = async (m, { conn, args, usedPrefix, command }) => {
-    if (command === 'facebook') {
-        if (!args[0]) return m.reply('*`Ingresa un enlace de facebook`*');
+  if (!args[0]) return m.reply('*`Ingresa un enlace de facebook`*');
 
-        try {
-            const apiResponse = await fetch(`https://thepapusapifacebook.onrender.com/api/fbvideodownload?url=${args[0]}`);
-            const responseData = await apiResponse.json();
+    try {
+        const apiResponse = await fetch(`https://delirius-api-oficial.vercel.app/api/tiktok?url=${args[0]}`);
+        const responseData = await apiResponse.json();
+        
+        if (responseData.status) {
+            const { data } = responseData;
+            const { title, links } = data;
+        let txt = '    `Facebook downloader`\n\n';
+            txt += `> *Titulo* : _${title}_\n`;
 
-            if (responseData.status) {
-                const { data } = responseData;
-                const { title, links } = data;
-
-                let listSections = [];
-                listSections.push({
-                    title: `Descargar Video TikTok: ${title}`,
-                    rows: [
-                        {
-                            header: 'VIDEO CALIDAD NORMAL',
-                            title: ` *${links.sd_0.size}*`,
-                            description: ``,
-                            id: `${usedPrefix}a ${args[0]}`
-                        },
-                        {
-                            header: 'VIDEO CALIDAD HD',
-                            title: ` *${links.hd_0.size}*`,
-                            description: ``,
-                            id: `${usedPrefix}b ${args[0]}`
-                        }
-                    ]
-                });
-
-                await conn.sendList(m.chat, '  ≡ *Calidades*', ``, `Click Aqui`, null, listSections, m);
-
-            } else {
-            }
-        } catch {
+            await conn.sendMessage(m.chat, { video: { url: links.sd_0.link;}, caption: txt }, { quoted: m });
+        } else {
         }
-    } else if (['a', 'b'].includes(command)) {
-        try {
-            const apiResponse = await fetch(`https://thepapusapifacebook.onrender.com/api/fbvideodownload?url=${args[0]}`);
-            const responseData = await apiResponse.json();
+    } catch {
+    }
+}
 
-            if (responseData.status) {
-                const { data } = responseData;
-                const { title, links } = data;
+handler.help = ['facebook *<link>*'];
+handler.tags = ['downloader'];
+handler.command = ['fb', 'facebook'];
+export default handler
 
-                let txt = `Descarga de video: ${title}\n\n`;
 
-                let mediaUrl;
-                if (command === 'a') {
                     mediaUrl = links.sd_0.link;
                     txt += `Calidad Normal`;
                 } else if (command === 'b') {
                     mediaUrl = links.hd_0.link;
                     txt += `Calidad HD`;
                 }
-
-                await conn.sendFile(m.chat, mediaUrl, 'video.mp4', txt, m);
-            } else {
-                m.reply('No se pudo obtener el video de TikTok.');
-            }
-        } catch (error) {
-            console.error(error);
-            m.reply('Ocurrió un error al intentar descargar el video de TikTok.');
-        }
-    }
-};
-
-handler.help = ['facebook *<enlace>*'];
-handler.tags = ['dl'];
-handler.command = ['facebook', 'a', 'b'];
-export default handler;
