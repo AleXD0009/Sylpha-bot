dab97c9fe523ab2a978f9bc00bd9cb802093f7c9
J̶T̶X̶S̶ <145930757+AleXD0009@users.noreply.github.com>
2024-07-14 13:22:01 -0300

    𝙎𝙮𝙡𝙥𝙝𝙖 - 𝘽𝙤𝙩

--- plugins/dl-facebook.js
+++ plugins/dl-facebook.js
@@ -1,6 +1,84 @@
 import fetch from 'node-fetch';
 
 let handler = async (m, { conn, args, usedPrefix, command }) => {
+  if (command === 'facebook') {
+    if (!args[0]) return m.reply('*`Ingresa un enlace de Facebook`*');
+
+    try {
+      await m.react('🕓');
+
+      const apiResponse = await fetch(`https://thepapusapifacebook.onrender.com/api/fbvideodownload?url=${args[0]}`);
+      const responseData = await apiResponse.json();
+
+      if (responseData.success) {
+        const { title, links } = responseData;
+
+        let listSections = [];
+        listSections.push({
+          title: '≡ Calidades',
+          rows: [
+            {
+              header: '360p',
+              title: '',
+              description: '',
+              id: `${usedPrefix}a ${args[0]}`
+            },
+            {
+              header: '560p',
+              title: '',
+              description: '',
+              id: `${usedPrefix}b ${args[0]}`
+            },
+            {
+              header: '720p',
+              title: '',
+              description: '',
+              id: `${usedPrefix}c ${args[0]}`
+            }
+          ]
+        });
+
+        await conn.sendList(m.chat, 'Facebook Downloader', `Descarga el video de "${title}"`, 'Haz clic aquí', null, listSections, m);
+
+        let txt = '    `Facebook downloader`\n\n';
+        txt += `> *Titulo* : _${title}_\n`;
+
+        let mediaUrl;
+
+        if (['a', 'b', 'c'].includes(command)) {
+          if (command === 'a') {
+            mediaUrl = links.find(link => link.quality === 'sd_0')?.link;
+          } else if (command === 'b') {
+            mediaUrl = links.find(link => link.quality === 'hd_0')?.link;
+          } else if (command === 'c') {
+            mediaUrl = links.find(link => link.quality === 'render_720p_0')?.link;
+          }
+        }
+
+        if (mediaUrl) {
+          await conn.sendMessage(m.chat, { video: { url: mediaUrl }, caption: txt }, { quoted: m });
+          await m.react('✅');
+        } else {
+          await m.react('❌');
+        }
+      } else {
+      }
+
+    } catch {
+      await m.react('❌');
+    }
+  }
+}
+
+handler.help = ['facebook *<link>*'];
+handler.tags = ['downloader'];
+handler.command = ['facebook', 'a', 'b', 'c'];
+export default handler;
+
+
+/*import fetch from 'node-fetch';
+
+let handler = async (m, { conn, args, usedPrefix, command }) => {
   if (!args[0]) return m.reply('*`Ingresa un enlace de facebook`*');
 
   try {
@@ -25,4 +103,4 @@ await m.react('❌');
 handler.help = ['facebook *<link>*'];
 handler.tags = ['downloader'];
 handler.command = ['fb', 'facebook'];
-export default handler;
+export default handler;*/
