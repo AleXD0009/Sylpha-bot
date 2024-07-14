c10bd58bc129b3d842376cbb5794f3b58055ad3f
J̶T̶X̶S̶ <145930757+AleXD0009@users.noreply.github.com>
2024-07-14 12:35:18 -0300

    𝙎𝙮𝙡𝙥𝙝𝙖 - 𝘽𝙤𝙩

--- plugins/dl-facebook.js
+++ plugins/dl-facebook.js
@@ -4,6 +4,7 @@ let handler = async (m, { conn, args, usedPrefix, command }) => {
   if (!args[0]) return m.reply('*`Ingresa un enlace de facebook`*');
 
   try {
+await m.react('🕓'); 
     const apiResponse = await fetch(`https://thepapusapifacebook.onrender.com/api/fbvideodownload?url=${args[0]}`);
     const responseData = await apiResponse.json();
     
@@ -13,9 +14,11 @@ let handler = async (m, { conn, args, usedPrefix, command }) => {
       txt += `> *Titulo* : _${title}_\n`;
 
       await conn.sendMessage(m.chat, { video: { url: links[0].link }, caption: txt }, { quoted: m });
+await m.react('✅'); 
     } else {
     }
   } catch {
+await m.react('❌'); 
   }
 }
 
