c530539d07c29fb0f40300ab99ee7fccb2ec1614
J̶T̶X̶S̶ <145930757+AleXD0009@users.noreply.github.com>
2024-07-15 09:09:41 -0300

    𝙎𝙮𝙡𝙥𝙝𝙖 - 𝘽𝙤𝙩

--- plugins/downloader-ytmp3.js
+++ plugins/downloader-ytmp3.js
@@ -1,14 +1,82 @@
-import fetch from 'node-fetch'
+import fetch from "node-fetch"
+import ytdl from "ytdl-core"
 
-let handler = async (m, { conn, args, usedPrefix, command, text }) => {
-  
-if (!args[0]) return m.reply(`*Ingrese un enlace de YouTube*`)
-  
-let res = await fetch("https://api.lolhuman.xyz/api/ytaudio?apikey=GataDiosV2&url="+args[0])
-let json = await res.json()
-  
-await conn.sendFile(m.chat, json.result.link.link, 'error.mp3', '', m, false, { mimetype: 'audio/mp4' })}
+let handler = async (m, {
+    conn,
+    args
+}) => {
+    if (!args[0]) throw "[ Masukkan Url Youtube! ]"
+    await conn.reply(m.chat, wait, m)
+    try {
 
-handler.command = /^(ytmp3)$/i
+        let Ytdl = await ytmp3(args[0])
+        let dls = "Download audio succes ( V1 )"
+        let ytthumb = await (await conn.getFile(Ytdl.meta.image)).data
+        let doc = {
+            audio: Ytdl.buffer,
+            mimetype: "audio/mp4",
+            fileName: Ytdl.meta.title,
+            contextInfo: {
+                externalAdReply: {
+                    showAdAttribution: true,
+                    mediaType: 2,
+                    mediaUrl: args[0],
+                    title: Ytdl.meta.title,
+                    body: dls,
+                    sourceUrl: args[0],
+                    thumbnail: ytthumb
+                }
+            }
+        }
+
+        await conn.sendMessage(m.chat, doc, {
+            quoted: m
+        })
+
+            } catch {
+            }
+        }
+handler.command = /^y((outube|tb)audio|(outube|tb?)mp3|utubaudio|taudio|ta)$/i
 
 export default handler
+
+async function ytmp3(url) {
+    try {
+        const {
+            videoDetails
+        } = await ytdl.getInfo(url, {
+            lang: "id"
+        });
+
+        const stream = ytdl(url, {
+            filter: "audioonly",
+            quality: 140
+        });
+        const chunks = [];
+
+        stream.on("data", (chunk) => {
+            chunks.push(chunk);
+        });
+
+        await new Promise((resolve, reject) => {
+            stream.on("end", resolve);
+            stream.on("error", reject);
+        });
+
+        const buffer = Buffer.concat(chunks);
+
+        return {
+            meta: {
+                title: videoDetails.title,
+                channel: videoDetails.author.name,
+                seconds: videoDetails.lengthSeconds,
+                description: videoDetails.description,
+                image: videoDetails.thumbnails.slice(-1)[0].url,
+            },
+            buffer: buffer,
+            size: buffer.length,
+        };
+    } catch (error) {
+        throw error;
+    }
+};
