6f2a6bcc7552029027c8d3911241ca58896e5a23
J̶T̶X̶S̶ <145930757+AleXD0009@users.noreply.github.com>
2024-07-15 08:58:21 -0300

    𝙎𝙮𝙡𝙥𝙝𝙖 - 𝘽𝙤𝙩

--- plugins/downloader-play.js
+++ plugins/downloader-play.js
@@ -1,12 +1,6 @@
 import yts from 'yt-search';
-import { youtubedl, youtubedlv2 } from '@bochilteam/scraper';
-import fetch from 'node-fetch';
-
-const limit = 100;
 
 let handler = async (m, { conn, command, args, text, usedPrefix }) => {
-    try {
-        if (command === 'play' || command === 'play2') {
             if (!text) {
                 return conn.reply(m.chat, '*Ingresa el nombre de lo que quieres buscar*', m);
             }
@@ -28,62 +22,12 @@ let handler = async (m, { conn, command, args, text, usedPrefix }) => {
             txt += `> *Visitas* : _${views.toLocaleString()}_\n`;
             txt += `> *Link* : _https://www.youtube.com/watch?v=${videoId}_\n`;
 
-            await conn.sendButton2(m.chat, txt, 'Nombre del bot', thumbnail, [
+            await conn.sendButton2(m.chat, txt, author, thumbnail, [
                 ['Audio', `${usedPrefix}ytmp3 ${url}`],
                 ['Video', `${usedPrefix}ytmp4 ${url}`]
             ], null, [['Canal', 'Nombre del canal']], m);
             await m.react('✅');
         }
-
-        if (command === 'ytmp3') {
-            if (!args || !args[0]) {
-                return conn.reply(m.chat, '`Ingresa el enlace de un video de YouTube`', m);
-            }
-
-            if (!args[0].match(/youtu/gi)) {
-                return conn.reply(m.chat, 'Verifica que el enlace sea de YouTube.', m);
-            }
-
-            let q = '128kbps';
-            await m.react('🕓');
-
-            try {
-                let v = args[0];
-                let yt = await youtubedl(v).catch(async () => await youtubedlv2(v));
-
-                if (!yt || !yt.audio || !yt.audio[q]) {
-                    throw `Error: No se pudo obtener la información del audio`;
-                }
-
-                let dl_url = await yt.audio[q].download();
-                let title = await yt.title;
-                let size = await yt.audio[q].fileSizeH;
-                let thumbnail = await yt.thumbnail;
-
-                let img = await (await fetch(thumbnail)).buffer();
-                if (size.split('MB')[0] >= limit) {
-                    return conn.reply(m.chat, `El archivo pesa más de ${limit} MB, se canceló la descarga.`, m);
-                }
-
-                let txt = '`Youtube - Download`\n\n';
-                txt += `*Titulo* : ${title}\n`;
-                txt += `*Calidad* : ${q}\n`;
-                txt += `*Tamaño* : ${size}\n\n`;
-                txt += `> *- ↻ El audio se está enviando, espera un momento, soy lenta...*`;
-
-                await conn.sendFile(m.chat, img, 'thumbnail.jpg', txt, m, null, { mimetype: 'image/jpeg' });
-                await conn.sendMessage(m.chat, { audio: { url: dl_url }, fileName: title + '.mp3', mimetype: 'audio/mp3' }, { quoted: m });
-                await m.react('✅');
-            } catch (error) {
-                console.error(error);
-                await m.react('❌');
-            }
-        }
-    } catch (error) {
-        console.error(error);
-        await m.react('❌');
-    }
-};
 
 handler.help = ['play', 'play2', 'ytmp3'];
 handler.tags = ['dl'];
