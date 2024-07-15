f571f2046a621c8449d98b46200ea4a182ba3e68
J̶T̶X̶S̶ <145930757+AleXD0009@users.noreply.github.com>
2024-07-15 09:03:29 -0300

    𝙎𝙮𝙡𝙥𝙝𝙖 - 𝘽𝙤𝙩

---
+++ plugins/downloader-ytmp3.js
@@ -0,0 +1,12 @@
+import fetch from 'node-fetch'
+
+let handler = async (m, { conn, args, usedPrefix, command, text }) => {
+if (!args[0]) throw `*Ingrese un enlace de YouTube*`
+let res = await fetch("https://api.lolhuman.xyz/api/ytaudio?apikey=GataDiosV2&url="+args[0])
+let json = await res.json()
+  
+await conn.sendFile(m.chat, json.result.link.link, 'error.mp3', '', m, false, { mimetype: 'audio/mp4' })}
+
+handler.command = /^(ytmp3)$/i
+
+export default handler
