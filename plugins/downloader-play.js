2bc08d56998ab07a90cc9ed04f20cf8bb5e4d37f
J̶T̶X̶S̶ <145930757+AleXD0009@users.noreply.github.com>
2024-07-15 06:31:16 -0300

    𝙎𝙮𝙡𝙥𝙝𝙖 - 𝘽𝙤𝙩

--- plugins/downloader-play.js
+++ plugins/downloader-play.js
@@ -2,37 +2,40 @@ import { ytmp3, ytmp4 } from '@ruhend/scraper';
 import yts from 'yt-search';
 
 let handler = async (m, { conn, usedPrefix, text, args, command }) => {
-if (command === 'play') {
-  
-if (!text) { return conn.reply(m.chat, '*`Ingresa El Nombre De Lo Que Quieres Buscar`*', m); }
+    if (command === 'play') {
+        if (!text) {
+            return conn.reply(m.chat, '*Ingresa el nombre de lo que quieres buscar*', m);
+        }
 
-let result = await yts(text);
-let ytres = result.videos;
+        try {
+            let result = await yts(text);
+            let vid = result.videos[0];
 
-    let res = await yts(text);
-    let vid = res.videos[0];
-    if (!vid) {
-        throw new Error('Resultado no encontrado');
+            if (!vid) {
+                throw new Error('Resultado no encontrado');
+            }
+
+            let { title, description, thumbnail, videoId, timestamp, views, ago, url } = vid;
+
+            let txt = '    `𝐘𝐎𝐔𝐓𝐔𝐁𝐄 𝐃𝐋`\n\n';
+            txt += `> *Título* : _${title}_\n`;
+            txt += `> *Creado* : _${ago}_\n`;
+            txt += `> *Duración* : _${timestamp}_\n`;
+            txt += `> *Visitas* : _${views.toLocaleString()}_\n`;
+
+            await conn.sendButton2(m.chat, [
+                [txt, thumbnail, '©️  ', [
+                    { button: 'Audio 🎶', url: `${usedPrefix}ytmp3 ${url}` },
+                    { button: 'Video 🎥', url: `${usedPrefix}ytmp4 ${url}` },
+                ]],
+            ], m);
+        } catch {
+        }
     }
-    let { title, description, thumbnail, videoId, timestamp, views, ago, url } = vid;
-  
-  
-        let txt = '    `𝐘𝐎𝐔𝐓𝐔𝐁𝐄 𝐃𝐋`\n\n';
-        txt += `> *Título* : _${vid.title}_\n`;
-        txt += `> *Creado* : _${vid.ago}_\n`
-        txt += `> *Duracion* : _${vid.timestamp}_\n`
-        txt += `> *Visitas : _${vid.views.toLocaleString()}_\n`
-  
-    await conn.sendButton2(m.chat, [
-        [txt, author, thumbnail, [
-            ['Audio 🎶', `${usedPrefix}ytmp3 ${url}`],
-            ['Video 🎥', `${usedPrefix}ytmp4 ${url}`],
-        ]],
-    ], m);
- }
-}
-handler.help = ['play']
-handler.tags = ['dl']
-handler.command = ['play'] 
+};
 
-export default handler
+handler.help = ['play'];
+handler.tags = ['dl'];
+handler.command = ['play'];
+
+export default handler;
