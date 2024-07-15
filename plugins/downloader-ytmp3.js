829ca1cf80810eb5e4088967353b61f646076881
J̶T̶X̶S̶ <145930757+AleXD0009@users.noreply.github.com>
2024-07-15 09:30:27 -0300

    𝙎𝙮𝙡𝙥𝙝𝙖 - 𝘽𝙤𝙩

--- plugins/downloader-ytmp3.js
+++ plugins/downloader-ytmp3.js
@@ -1,34 +1,69 @@
-import { youtubedl, youtubedlv2 } from '@bochilteam/scraper';
 
+import ytdl from 'ytdl-core'
+import fs from 'fs'
+import { pipeline } from 'stream'
+import { promisify } from 'util'
+import os from 'os'
+import fg from 'api-dylux'
+import fetch from 'node-fetch'
 let handler = async (m, { conn, text, args, isPrems, isOwner, usedPrefix, command }) => {
   if (!args || !args[0]) return conn.reply(m.chat, `✳️ Example :\n${usedPrefix + command} https://youtu.be/YzkTFFwxtXI`, m)
-  if (!args[0].match(/youtu/gi)) throw `❎ Verify that it is a YouTube link.`;
+  if (!args[0].match(/youtu/gi)) throw ``
+ let chat = global.db.data.chats[m.chat]
+ let q = '128kbps'
+ 
+ try {
 
-
+		const yt = await fg.yta(args[0]) 
+		let { title, dl_url, quality, size, sizeB } = yt
+		
+		conn.sendFile(m.chat, dl_url, title + '.mp3', `
+  ${title}  ${size}
+`.trim(), m, false, { mimetype: 'audio/mpeg', asDocument: chat.useDocument })
+		m.react(done)
+ 	} catch {
   try {
-    let q = '128kbps'; 
-    let v = args[0]; 
-    const yt = await youtubedl(v).catch(async () => await youtubedlv2(v)); 
-    const dl_url = await yt.audio[q].download(); 
-    const title = await yt.title; 
+  	
+	const { title, dl_url } = await ytmp3(args[0]);
+  
+		conn.sendFile(m.chat, dl_url, title + '.mp3', `
+ ≡  *YTDL 2** : ${title}
+`.trim(), m, false, { mimetype: 'audio/mpeg', asDocument: chat.useDocument })
+		m.react(done)
+        } catch {
+			await m.reply(`❎`)
+} 
+}
 
-    conn.sendFile(
-      m.chat,
-      dl_url,
-      title + '.mp3',
-      null, 
-      m,
-      false,
-      { mimetype: 'audio/mpeg' }
-    );
-
-  } catch {
-    await m.reply(`❎ Error: Could not download the audio.`)
-  }
-};
-
+}
 handler.help = ['ytmp3 <url>']
-handler.tags = ['downloader']
-handler.command = ['ytmp3', 'yta'] 
+handler.tags = ['dl']
+handler.command = ['ytmp3', 'fgmp3'] 
+handler.diamond = false
 
 export default handler
+
+const streamPipeline = promisify(pipeline);
+
+async function ytmp3(url) {
+    const videoInfo = await ytdl.getInfo(url);
+    const { videoDetails } = videoInfo;
+    const { title, thumbnails, lengthSeconds, viewCount, uploadDate } = videoDetails;
+    const thumbnail = thumbnails[0].url;
+    
+    const audioStream = ytdl(url, { filter: 'audioonly', quality: 'highestaudio' });
+    const tmpDir = os.tmpdir();
+    const audioFilePath = `${tmpDir}/${title}.mp3`;
+
+    await streamPipeline(audioStream, fs.createWriteStream(audioFilePath));
+
+    return {
+        title,
+        views: viewCount,
+        publish: uploadDate,
+        duration: lengthSeconds,
+        quality: '128kbps',
+        thumb: thumbnail,
+        dl_url: audioFilePath
+    };
+}
