fc642d0fd85da2d066966a5f1cce8493e32d3283
JTxs <145930757+AleXD0009@users.noreply.github.com>
2024-07-31 18:39:35 -0300

    .

--- plugins/downloader-tiktok.js
+++ plugins/downloader-tiktok.js
@@ -2,41 +2,26 @@ import { ttdl } from '@ruhend/scraper';
 
 let handler = async (m, { conn, args, usedPrefix, command }) => {
  if (!args || !args[0]) return conn.reply(m.chat, '*`Ingresa un enlace de tiktok`*', m, )
-    try {
+ 
+try {
 await m.react('🕓'); 
-        let {
-            title,
-            author,
-            username,
-            published,
-            like,
-            comment,
-            share,
-            views,
-            bookmark,
-            video,
-            cover,
-            duration,
-            music,
-            profilePicture
-        } = await ttdl(args[0]);
+let { title, author, username, published, like, comment, share, views, bookmark, video, cover, duration, music, profilePicture } = await ttdl(args[0]);
 
-        let txt = '    `Tiktok downloader`\n\n';
-        txt += `> *Título* : _${title}_\n`;
-        txt += `> *Autor* : _${author}_\n`;
-        txt += `> *Duración* : _${duration}_\n`;
-        txt += `> *Vistas* : _${views}_\n`;
-        txt += `> *Likes* : _${like}_\n`; 
-        txt += `> *Comentarios* : _${comment}_\n`;
-        txt += `> *Compartidos* : _${share}_\n`;
-        txt += `> *Publicado* : _${published}_\n`;
+let txt = '*`T I K T O K - D L`*\n\n';
+txt += `> _Título_ : *${title || 'X'}*\n`;
+txt += `> _Autor_ : *${author || 'X'}*\n`;
+txt += `> _Duración_ : *${duration || 'X'}*\n`;
+txt += `> _Vistas_ : *${views || 'X'}*\n`;
+txt += `> _Likes_ : *${like || 'X'}*\n`; 
+txt += `> _Comentarios_ : *${comment || 'X'}*\n`;
+txt += `> _Compartidos_ : *${share || 'X'}*\n`;
+txt += `> _Publicado_ : *${published || 'X'}*\n`;
 
-        await conn.sendFile(m.chat, video, 'tiktok.mp4', txt, m, null, rcanal);
-        await m.react('✅'); 
+await conn.sendFile(m.chat, video, 'tiktok.mp4', txt, m, null, rcanal);
+await m.react('✅'); 
     } catch {
-        await m.react('❌'); 
-    }
-};
+await m.react('❌'); 
+}}
 
 handler.help = ['tiktok *<link>*']
 handler.tags = ['dl'] 
