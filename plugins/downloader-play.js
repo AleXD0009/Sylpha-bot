f190aa2dd2a6c0db7b435396d99c1d17fc91e741
JTxs <145930757+AleXD0009@users.noreply.github.com>
2024-07-31 18:29:50 -0300

    .

--- plugins/downloader-play.js
+++ plugins/downloader-play.js
@@ -1,36 +1,32 @@
 import yts from 'yt-search';
 
 let handler = async (m, { conn, command, args, text, usedPrefix }) => {
-            if (!text) {
-                return conn.reply(m.chat, '*Ingresa el nombre de lo que quieres buscar*', m);
-            }
+if (!text) return conn.reply(m.chat, '_Ingresa el nombre de lo que quieres buscar_', m);
 
-            await m.react('🕓');
-            let res = await yts(text);
-            let play = res.videos[0];
+await m.react('🕓');
+let res = await yts(text);
+let play = res.videos[0];
 
-            if (!play) {
-                throw `Error: Vídeo no encontrado`;
-            }
+if (!play) return conn.reply(m.chat, `No se encontraron resultados`, m)
 
-            let { title, thumbnail, ago, timestamp, views, videoId, url } = play;
+let { title, thumbnail, ago, timestamp, views, videoId, url } = play;
 
-            let txt = '`𝐘𝐨𝐮𝐭𝐮𝐛𝐞 - 𝐃𝐨𝐰𝐧𝐥𝐨𝐚𝐝`\n\n';
-            txt += `> *Título* : _${title}_\n`;
-            txt += `> *Creado* : _${ago}_\n`;
-            txt += `> *Duración* : _${timestamp}_\n`;
-            txt += `> *Visitas* : _${views.toLocaleString()}_\n`;
-            txt += `> *Link* : _https://www.youtube.com/watch?v=${videoId}_\n`;
+let txt = '*`P L A Y - D L`*\n\n';
+txt += `> _Título_ : *${title || 'x'}*\n`;
+txt += `> _Creado_ : *${ago || 'x'}*\n`;
+txt += `> _Duración_ : *${timestamp || 'x'}*\n`;
+txt += `> _Visitas_ : *${views.toLocaleString() || 'x'}*\n`;
+txt += `> _Link_ : *https://www.youtube.com/watch?v=${videoId}*\n`;
 
-            await conn.sendButton2(m.chat, txt, author, thumbnail, [
-                ['Audio', `${usedPrefix}ytmp3 ${url}`],
-                ['Video', `${usedPrefix}ytmp4 ${url}`]
-            ], null, [['Canal', 'Nombre del canal']], m);
-            await m.react('✅');
-        }
+await conn.sendButton2(m.chat, txt, author, thumbnail, [
+['Audio', `${usedPrefix}ytmp3 ${url}`],
+['Video', `${usedPrefix}ytmp4 ${url}`]
+], null, [['Canal', canal]], m);
+await m.react('✅')
+}
 
-handler.help = ['play', 'play2', 'ytmp3'];
+handler.help = ['play', 'play2']
 handler.tags = ['dl'];
-handler.command = ['play', 'play2'];
+handler.command = ['play', 'play2']
 
 export default handler;
