45e9c6ced99dec01433cb4d83497f7a91434a4f1
JTxs <145930757+AleXD0009@users.noreply.github.com>
2024-08-12 09:39:06 -0300

    .

--- plugins/_fakeReply.js
+++ plugins/_fakeReply.js
@@ -2,16 +2,14 @@ import fetch from 'node-fetch'
 
 export async function before(m, { conn }) {
 
- global.rcanal = {
-    contextInfo: {
-    	isForwarded: true,
-    forwardedNewsletterMessageInfo: {
-      newsletterJid: id_canal,
-      serverMessageId: 100,
-      newsletterName: name_canal,
-    },
-    },
-  }
+global.rcanal = {
+contextInfo: {
+isForwarded: true,
+forwardedNewsletterMessageInfo: {
+newsletterJid: id_canal,
+serverMessageId: 100,
+newsletterName: name_canal,
+}}}
 
 /* global.adReply = {
 	    contextInfo: { 
