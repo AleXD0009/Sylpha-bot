ac7c68023ba060ac8188a750d1e6d741f3614e08
JTxs <145930757+AleXD0009@users.noreply.github.com>
2024-08-12 09:44:14 -0300

    .

--- plugins/dl-instagram.js
+++ plugins/dl-instagram.js
@@ -1,14 +1,20 @@
 import Starlights from '@StarlightsTeam/Scraper'
 
 let handler = async (m, { conn, args, usedPrefix, command }) => {
-if (!args[0]) return conn.reply(m.chat, '*`Ingresa el enlace del vídeo de Instagram junto al comando.`*', m)
+
+if (!args[0]) return conn.reply(m.chat, '*Ingresa el enlace del vídeo de Instagram junto al comando*', m)
+
 try {
+m.react('🕑')
 let { dl_url } = await Starlights.igdl(args[0])
 await conn.sendFile(m.chat, dl_url, 'igdl.mp4', null, m, null, rcanal)
+m.react('✅')
 } catch {
+m.react('❌')
 }}
+
 handler.help = ['instagram *<link>*']
 handler.tags = ['dl']
-handler.command = /^(instagramdl|instagram|igdl|ig|instagramdl2|instagram2|igdl2|ig2|instagramdl3|instagram3|igdl3|ig3)$/i
+handler.command = ['instagramdl', 'instagram', 'igdl', 'ig']
 
 export default handler
