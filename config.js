5598161192d7e11a3282765288887d858caf724b
JTxs <145930757+AleXD0009@users.noreply.github.com>
2025-01-04 11:51:41 -0300

    🚩

--- config.js
+++ config.js
@@ -2,61 +2,29 @@ import { watchFile, unwatchFile } from 'fs'
 import chalk from 'chalk'
 import { fileURLToPath } from 'url'
 import fs from 'fs'
-import cheerio from 'cheerio'
-import fetch from 'node-fetch'
-import axios from 'axios'
-import moment from 'moment-timezone' 
-
-//─────「 NUMEROS - OWNERS 」─────`*
 
 global.owner = [
-  ['5491126788746', 'JTxs', true],
-  ['51913091648', 'Gabriel Curi', true],
+  ['', '-', true],
   ['']
 
 ]
 
-//─────「 NUMEROS - MODS | PREMS 」─────`*
-
 global.mods = []
 global.prems = []
    
-//─────「 STICKERS | NOMBRES | CANAL 」─────`*
 
 global.packname = ``
-global.author = '@ 2024 JTxs | All rigths reserved'
+global.author = ''
 
 global.botname = 'Sylpha - Bot'
 
-global.name_canal = '@ 2024 JTxs | All rigths reserved'
+global.name_canal = '-'
 global.id_canal = '120363274577422945@newsletter'
-
-//─────「 IMAGENES 」─────`*
-
-global.catalogo = fs.readFileSync('./storage/img/catalogo.png')
-//global.miniurl = fs.readFileSync('./storage/img/miniurl.jpg')
-
-//─────「 OTROS 」─────`*
-
-global.estilo = { key: {  fromMe: false, participant: `0@s.whatsapp.net`, ...(false ? { remoteJid: "5219992095479-1625305606@g.us" } : {}) }, message: { orderMessage: { itemCount : -999999, status: 1, surface : 1, message: botname, orderTitle: 'Bang', thumbnail: catalogo, sellerJid: '0@s.whatsapp.net'}}}
-
-//─────「 LINKS 」─────`*
 
 global.canal = 'https://whatsapp.com/channel/0029VaeQcFXEFeXtNMHk0D0n'
 
-//─────「  OTROS 」─────`*
-
-global.cheerio = cheerio
-global.fs = fs
-global.fetch = fetch
-global.axios = axios
-global.moment = moment	
-
-//─────「─────」─────`*
-
 global.multiplier = 69 
-global.maxwarn = '2' // máxima advertencias
-
+global.maxwarn = '2'
 //─────「─────」─────`*
 
 let file = fileURLToPath(import.meta.url)
