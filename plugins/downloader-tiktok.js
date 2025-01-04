de6d4b5e0838ac134a136e506a91e99fed0b1804
JTxs <145930757+AleXD0009@users.noreply.github.com>
2025-01-04 11:32:07 -0300

    🐱

--- plugins/downloader-tiktok.js
+++ plugins/downloader-tiktok.js
@@ -1,30 +1,28 @@
-import { ttdl } from '@ruhend/scraper';
+import { ttdl } from 'ruhend-scraper'
 
 let handler = async (m, { conn, args, usedPrefix, command }) => {
- if (!args || !args[0]) return conn.reply(m.chat, '*`Ingresa un enlace de tiktok`*', m, )
- 
+if (!args[0]) return conn.reply(m.chat, 'Ingresa un enlace de *tiktok*', m)
+
+ await m.react('🕓')
 try {
-await m.react('🕓'); 
-let { title, author, username, published, like, comment, share, views, bookmark, video, cover, duration, music, profilePicture } = await ttdl(args[0]);
+let { title, author, username, published, like, comment, share, views, bookmark, video, cover, duration, music, profilePicture } = await ttdl(args[0])
 
-let txt = '';
-txt += `> _Título_ : *${title || '❌'}*\n`;
-txt += `> _Autor_ : *${author || '❌'}*\n`;
-txt += `> _Duración_ : *${duration || '❌'}*\n`;
-txt += `> _Vistas_ : *${views || '❌'}*\n`;
-txt += `> _Likes_ : *${like || '❌'}*\n`; 
-txt += `> _Comentarios_ : *${comment || '❌'}*\n`;
-txt += `> _Compartidos_ : *${share || '❌'}*\n`;
-txt += `> _Publicado_ : *${published || '❌'}*\n`;
+let txt = `- *Título :* ${title || '-'}
+- *Autor :* ${author || '-'}
+- *Duración :* ${duration || '-'}
+- *Vistas :* ${views || '-'}
+- *Likes :* ${like || '-'}
+- *Comentarios :* ${comment || '-'}
+- *Publicado :* ${published || '-'}`
 
-await conn.sendFile(m.chat, video, 'tiktok.mp4', txt, m, null, rcanal);
-await m.react('✅'); 
-    } catch {
-await m.react('❌'); 
+await conn.sendFile(m.chat, video, 'Tiktokdl.mp4', txt, m)
+await m.react('✅')
+} catch {
+await m.react('❌') 
 }}
 
 handler.help = ['tiktok *<link>*']
 handler.tags = ['dl'] 
 handler.command = ['tiktok', 'tt']
 
-export default handler;
+export default handler
