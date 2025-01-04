b02169f7b88eb85e0211957b5dd55a10fbc85415
JTxs <145930757+AleXD0009@users.noreply.github.com>
2025-01-04 04:15:15 -0300

    🐢

--- plugins/downloader-ytmp3.js
+++ plugins/downloader-ytmp3.js
@@ -1,26 +1,20 @@
-import { ytmp3, ytmp3v3 } from 'ruhend-scraper'
+import { ytmp3 } from 'ruhend-scraper'
 
-let handler = async (m, { conn, args, text, usedPrefix, command }) => {
-if (!args || !args[0]) return conn.reply(m.chat, '*_Ingresa un enlace de Youtube_*', m)
-try{
+let handler = async (m, { conn, args, text }) => {
+if (!args[0]) return conn.reply(m.chat, 'Ingresa un enlace de *Youtube*', m)
+
 await m.react('🕑')
+try{
 let data = await ytmp3(args[0])
 let { audio, title } = data
 conn.sendFile(m.chat, audio, title + '.mp3', `${title}`.trim(), m, false, { mimetype: 'audio/mpeg', asDocument: false })
 await m.react('✅')
 } catch {
-try {
-await m.react('🕑')
-let data = await ytmp3v3(args[0])
-let { audio, title } = data
-conn.sendFile(m.chat, audio, title + '.mp3', `${title}`.trim(), m, false, { mimetype: 'audio/mpeg', asDocument: false })
-await m.react('✅')
-} catch {
 await m.react('❌')
-}}}
+}}
 
 handler.help = ['ytmp3 *<link>*']
 handler.tags = ['dl']
-handler.command = ['ytmp3', 'yta', 'ytaudio']
+handler.command = ['ytmp3', 'yta'']
 
 export default handler
