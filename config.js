4e47877c9fbf0a46b02fc6bec4fdfd98ed858a93
JTxs <145930757+AleXD0009@users.noreply.github.com>
2025-02-19 01:14:47 -0300

    🐢

--- config.js
+++ config.js
@@ -1,31 +1,22 @@
 import { watchFile, unwatchFile } from 'fs' 
 import chalk from 'chalk'
 import { fileURLToPath } from 'url'
-import fs from 'fs'
 
-global.owner = [
-  ['', '-', true],
-  ['']
-
-]
-
+global.owner = [ ['', '-', true], [''] ]
 global.mods = []
 global.prems = []
    
 
 global.packname = ``
 global.author = ''
-
 global.botname = 'Sylpha - Bot'
 
 global.name_canal = '-'
 global.id_canal = '120363274577422945@newsletter'
-
 global.canal = 'https://whatsapp.com/channel/0029VaeQcFXEFeXtNMHk0D0n'
 
 global.multiplier = 69 
 global.maxwarn = '2'
-//─────「─────」─────`*
 
 let file = fileURLToPath(import.meta.url)
 watchFile(file, () => {
